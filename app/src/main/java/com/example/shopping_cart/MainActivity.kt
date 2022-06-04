d5b88a9f893186d4d1f98aada9c98baefff1cc69
carly-west <carly.west@byui.edu>
2022-06-04 12:04:28 -0600

    Changes

--- app/src/main/java/com/example/shopping_cart/MainActivity.kt
+++ app/src/main/java/com/example/shopping_cart/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Setting up all the variables
         var enterItemButton = findViewById<Button>(R.id.enter_item_button)
         var removeItemButton = findViewById<Button>(R.id.remove_item_button)
         var itemInput = findViewById<EditText>(R.id.add_item_input)
@@ -22,16 +23,30 @@ class MainActivity : AppCompatActivity() {
 
 
         enterItemButton.setOnClickListener {
+//            Puts the input to a variable string
             var string = itemInput.text.toString()
+
+//            Capitalizes the first letter of the string and adds it to the items list
             items.add(string.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
+
+//            Puts the item list to the textview
             itemList.text = items.toString()
+
+//            Clears the input field
             itemInput.text.clear()
         }
 
         removeItemButton.setOnClickListener {
+//            Puts the input to a variable string
             var string = itemInput.text.toString()
+
+//            Capitalizes the first letter of the string and removes it from the items list
             items.remove(string.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
+
+//            Puts the item list to the textview
             itemList.text = items.toString()
+
+//            Clears the input field
             itemInput.text.clear()
         }
 
